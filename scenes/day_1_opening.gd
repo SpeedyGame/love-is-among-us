2da69d5c3b6f9a9c6ac4f682c05a7f528f9fdd27
Steven Mensah <mensahsteven21@gmail.com>
2024-01-30 14:22:29 -0600

    created day1opening scene

---
+++ scenes/day_1_opening.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Dialogic.start("res://dialogic_assets/timelines/opening.dtl")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
