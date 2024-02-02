04c6ee333f0da135da9f45d397e12101b52bc310
Steven Mensah <mensahsteven21@gmail.com>
2024-02-01 20:42:47 -0600

    added title screen music, meeting music, slowed down text speed

--- scenes/title_screen.gd
+++ scenes/title_screen.gd
@@ -3,9 +3,12 @@ extends Node2D
 @export var opening_scene : PackedScene
 @export var transistioner : Transitioner
 
+@onready var audio_control : AnimationPlayer =  $TitleScreenMusic/AudioFade
+@onready var music_player : AudioStreamPlayer2D =  $TitleScreenMusic 	# $ is the name of the node.
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	music_player.play(0.0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,4 +18,5 @@ func _process(delta):
 
 func _on_start_pressed():
 	transistioner.set_next_animation(true)
+	audio_control.queue("fade_out_music")
 	# get_tree().change_scene_to_packed(opening_scene)
