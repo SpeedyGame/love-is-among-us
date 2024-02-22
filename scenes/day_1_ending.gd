e95e2d668abf7dc2ce9b2e1e0717530fd9626d9d
Steven Mensah <mensahsteven21@gmail.com>
2024-02-22 12:17:44 -0600

    day1end to day2 routing

---
+++ scenes/day_1_ending.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+#@export var transistioner : Transitioner
+@export var ptransistioner : PackedTransitioner
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Dialogic.start("res://dialogic_assets/timelines/day1_end.dtl")
+	Dialogic.signal_event.connect(DialogicSignal)
+
+func DialogicSignal(argument:String):
+	if argument == "day2":
+		ptransistioner.set_next_animation(true)
+		#print("Something was activated!")
