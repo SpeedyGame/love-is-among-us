4afcff2e6de5302c3bbd05412b25333f418e992d
LiamKeith <lkeith@skidmore.edu>
2024-03-07 12:35:54 -0600

    electricla work

---
+++ minigames/electrical game/permanent.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+var line = new Line2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	line.add_point(area_shape_index.position)
+
+
+func _on_area_2d_area_shape_exited(area_rid, area, area_shape_index, local_shape_index):
+	pass # Replace with function body.
+
+
+func draw(area_shape_index, local_shape_index):
+	draw_line(area_shape_index.position, local_shape_index.global_position, "black", 1)
